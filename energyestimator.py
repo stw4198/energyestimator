8dbacde583b2496e517e397578ef2cf8a0b9582c
Rob Foster <rjfoster1@sheffield.ac.uk>
2020-10-20 17:05:06 +0100

    General updates to energy estimator script

--- energyestimator.py
+++ energyestimator.py
@@ -2,9 +2,11 @@ import argparse
 import os
 import pdb
 import sys
+
 import numpy as np
 from scipy.interpolate import griddata, interp1d
 from tqdm import tqdm
+
 try:
     import ROOT as root
 except ImportError:
@@ -12,15 +14,15 @@ except ImportError:
     exit(1)
 
 # TODO: Option to create fit to energy and then calculate best guess
-# TODO: Add qfit compatibility
+# TODO: Option to write best guess energy to tree
 
 np.set_printoptions(suppress=True)
 
 class Estimator():
 
     def __init__(self, rat_fn=None, bonsai_fn=None, min_t=-3, max_t=6,
-                 dark_noise=3000., qe=0.3, pmt_sep=750., medium=None,
-                 geometry=None, load_lib=True):
+                 dark_noise=10000., qe=0.3, pmt_sep=750., medium=None,
+                 geometry=None, fitter=None, load_lib=True):
         self.rat_fn = rat_fn
         self.bonsai_fn = bonsai_fn
         self.min_t = min_t
@@ -28,11 +30,12 @@ class Estimator():
         self.window = self.max_t - self.min_t
         self.medium = medium
         self.geometry = geometry
-        self.media_optics = {"water": 117509,
-                             "wbls1pct": 145312,
-                             "wbls3pct": 135169,
-                             "wbls5pct": 126889
-                            }
+        self.fitter = fitter
+        self.media_optics = {
+            "water" : 117509,
+            "wbls1pct" : 145312,
+            "wbls3pct" : 135169,
+            "wbls5pct" : 126889}
         self.pmt_sep = pmt_sep
         self.dark_noise = dark_noise
         self.qe = qe
@@ -46,19 +49,28 @@ class Estimator():
                             containing reconstructed events", type=str)
         parser.add_argument("--minT", help="start time of rolling window \
                             relative to current PMT hit time in ns, typically \
-                            a negative number or zero", type=int, default=-3)
-        parser.add_argument("--maxT", help="end time of rolling window in ns",
+                            a negative number or zero \
+                            (default: -3)", type=int, default=-3)
+        parser.add_argument("--maxT", help="end time of rolling window in ns \
+                            (default: 6)",
                             type=int, default=6)
-        parser.add_argument("--medium", help="specify the detection medium",
+        parser.add_argument("--medium", help="specify the detection medium \
+                            (default: water)",
                             type=str, choices=['water', 'wbls1pct',
                                                'wbls3pct', 'wbls5pct'],
                             default='water')
-        parser.add_argument("--darkNoise", help="specify the PMT dark noise",
+        parser.add_argument("--darkNoise", help="specify the PMT dark noise \
+                            (default: 3000)",
                             type=float, default=3000.)
-        parser.add_argument("--qe", help="Specify the PMT quantum efficiency",
+        parser.add_argument("--qe", help="Specify the PMT quantum efficiency \
+                            (default: 0.3)",
                             type=float, default=0.3)
         parser.add_argument("--pmtSep", help="specify the average PMT \
-                            diagonal separation.", type=float, default=750.)
+                            diagonal separation, set to 0 to calculate dynamically \
+                            (default: 750)", type=float, default=750.)
+        parser.add_argument("--fitter", help="specify which fitter to analyse: \
+                            bonsai or qfit \
+                            (default: bonsai)", type=str, default='bonsai')
         args = parser.parse_args(argv)
         if self.load_lib:
             root.gSystem.Load('libRATEvent.so')
@@ -76,6 +88,7 @@ class Estimator():
         self.pmt_sep = args.pmtSep
         self.transparency = self.media_optics[self.medium]
         self.get_file_data()
+        if self.pmt_sep == 0: self.pmt_sep = self.calculate_pmt_separation()
 
     def get_file_data(self):
         self.rat_t = self.rat_file.Get("T")
@@ -90,6 +103,7 @@ class Estimator():
         self.pmtinfo = self.rat_rt.run.GetPMTInfo()
 
     def calculate_pmt_separation(self, buffer=50):
+        print("Calculating PMT separation from PMTInfo")
         pmt_pos = []
         for pmt in range(self.pmtinfo.GetPMTCount()):
             # All PMTs are type 1 in the files that I have
@@ -99,14 +113,16 @@ class Estimator():
         radial_sep = np.max(np.diff(np.unique(np.sqrt(pmt_pos[:,0]**2 + pmt_pos[:,1]**2))))
         z_sep = np.max(np.diff(np.unique(pmt_pos[:,2])))
         pmt_separation = np.sqrt(radial_sep**2 + z_sep**2)
+        print(f"Setting PMT separation to {pmt_separation + buffer}.")
         return pmt_separation + buffer
 
-    def eventloop(self, write_tree=True):
+    def eventloop(self, write_tree=True, fitter='bonsai'):
         r_nentry = self.rat_t.GetEntries()
         b_nentry = self.bonsai_t.GetEntries()
         geo_data = self.parse_geometric_corrections("geo_correction.csv")
         final_row, max_height = calculate_final_row_height(self.pmtinfo)
         nXeff_values = []
+        nX_values = []
         print(f"There are {b_nentry} reconstructed events in {self.bonsai_fn}")
         for b_entry, b_event in enumerate(tqdm(self.bonsai_t, total=b_nentry)):
             self.rat_t.GetEntry(b_event.mcid)
@@ -117,35 +133,42 @@ class Estimator():
             for ev_idx in range(evcount):
                 if ev_idx != b_event.subid:
                     continue
-                ev = self.rat_t.ds.GetEV(ev_idx)
-                hitpmtcount = ev.GetPMTCount()
                 assert b_event.mcx == self.rat_t.ds.GetMC().GetMCParticle(0).GetPosition().X()
-                reco_vertex = np.array([b_event.x, b_event.y, b_event.z])
+                if fitter == 'qfit':
+                    reco_vertex = np.array([b_event.xQFit, b_event.yQFit, b_event.zQFit])
+                else:
+                    reco_vertex = np.array([b_event.x, b_event.y, b_event.z])
                 if np.any(reco_vertex < -9998):
                     nXeff_values.append(-9999.9)
+                    nX_values.append(-9999.9)
                     continue
                 hit_pmts = []
+                ev = self.rat_t.ds.GetEV(ev_idx)
+                hitpmtcount = ev.GetPMTCount()
                 for pmt_idx in range(hitpmtcount):
                     pmt = ev.GetPMT(pmt_idx)
                     hit_pmts.append(pmt)
                 corrected_hit_times = correct_tof(reco_vertex, self.pmtinfo, hit_pmts)
                 nX, nXmask = nXhits(corrected_hit_times, self.min_t, self.max_t)
+                nX_values.append(nX)
                 n2X, _ = nXhits(corrected_hit_times, 2*self.min_t, 2*self.max_t)
                 nXPMTs = np.array(hit_pmts)[nXmask]
                 nXPMTpos = np.array([np.array(self.pmtinfo.GetPosition(pmt.GetID())) for pmt in nXPMTs])
                 nXeff = 0.
                 for hit in range(nX):
-                    #multihit_cor = occupancy(nXPMTpos, hit, nX, self.pmt_sep)
                     multihit_cor = new_occupancy(nXPMTpos, hit, self.pmt_sep, row_height=final_row, roof_height=max_height)
                     late_cor = tail_hits(nX, n2X, self.window, self.dark_noise)
                     dark_cor = dark_hits(nX, self.window, self.dark_noise)
-                    geo_cor = geometric1D(geo_data[:,0], geo_data[:,1], reco_vertex)
+                    #geo_cor = geometric1D(geo_data[:,0], geo_data[:,1], reco_vertex)
                     transp_cor = transparency(nXPMTpos[hit], reco_vertex, self.transparency)
                     qe_cor = quantum_efficiency(self.qe)
-                    nXeff += (multihit_cor + late_cor - dark_cor) * transp_cor * geo_cor * qe_cor
+                    nXeff += (multihit_cor + late_cor - dark_cor) * transp_cor * qe_cor
+                geo_cor = geometric1D(geo_data[:,0], geo_data[:,1], reco_vertex)
+                nXeff *= geo_cor
                 nXeff_values.append(nXeff)
         if write_tree:
-            self.write_to_tree(nXeff_values)
+            self.write_to_tree(nXeff_values, "n"+str((self.max_t-self.min_t))+"eff")
+            self.write_to_tree(nX_values, "n"+str((self.max_t-self.min_t))+"py")
                 
     @staticmethod
     def is_valid_file(parser, arg, type="READ"):
@@ -154,13 +177,19 @@ class Estimator():
         else:
             return root.TFile(arg, type)
 
-    def write_to_tree(self, values):
-        nXeff_value = np.empty((1), dtype="double")
-        branch = self.bonsai_t.Branch("nXeff", nXeff_value, "nXeff/D")
+    def write_to_tree(self, values, br_name):
+        # write values to bonsai tree under branch br_name
+        vals_to_write = np.empty((1), dtype="double")
+        branch = self.bonsai_t.Branch(br_name, vals_to_write, br_name+"/D")
         for value in values:
-            nXeff_value[0] = value
+            vals_to_write[0] = value
             branch.Fill()
         self.bonsai_file.Write("", root.TFile.kOverwrite)
+
+    def read_from_tree(self, br_name):
+        # Read values of branch br_name from bonsai tree into np array
+        values = self.bonsai_t.AsMatrix(columns=[br_name])
+        return values
 
     @staticmethod
     def parse_geometric_corrections(filename):
@@ -178,7 +207,10 @@ class Estimator():
 
     @staticmethod
     def estimate_energy(nXeff, fit):
+        # pass individual value or array
         estimate = fit[0] + (fit[1] * nXeff)
+        if isinstance(estimate, np.ndarray):
+            estimate[estimate<0] = -9999.9
         return estimate
 
 def correct_tof(vertex, pmtinfo, evPMTs, SoL=218):
